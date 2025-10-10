d1912d2c2b28b5961cfe6f48e5786c3dbfd6af4d
Alejandro <alejandro.laorden@proton.me>
2025-10-10 19:55:14 +0200

    Auto-update 2025-10-10 19:55:14

--- git_manager.bat
+++ git_manager.bat
@@ -41,6 +41,7 @@ echo Estado de los proyectos:
 wsl bash -c "cd '/mnt/c/DISCO DURO/proyectos/publicador/Publicador' && echo '=== Publicador ===' && git status --short && echo 'Rama:' $(git branch --show-current) && echo '---'"
 wsl bash -c "cd '/mnt/c/DISCO DURO/proyectos/subocasoft/subocasoft' && echo '=== Subocasoft ===' && git status --short && echo 'Rama:' $(git branch --show-current) && echo '---'"
 wsl bash -c "cd '/mnt/c/DISCO DURO/proyectos/factuzam/Factuzam' && echo '=== Factuzam ===' && git status --short && echo 'Rama:' $(git branch --show-current) && echo '---'"
+wsl bash -c "cd '/mnt/c/DISCO DURO/proyectos/OdaVeriFactu/OdaVeriFactu' && echo '=== OdaVeriFactu ===' && git status --short && echo 'Rama:' $(git branch --show-current) && echo '---'"
 echo.
 pause
 goto menu
